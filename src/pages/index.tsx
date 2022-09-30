162f58b79233c0b26e420c3d4bd05571774a73bc
Matheus Tanaka <matheustanakanog@gmail.com>
2022-09-30 20:30:56 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
-import styles from "../styles/home.module.scss"
+import Image from "next/image"
+import styles from "./home.module.scss"
+import img from '../../public/me.jpeg'
+import { FiGithub } from 'react-icons/fi'
+import { HiOutlineMail } from 'react-icons/hi'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World</h1>
+    <>
+    <section className={styles.homeContainer}>
+      <div className={styles.headerContent}>
+        <nav>
+          <a>
+            Home
+          </a>
+          <a>
+            Blog
+          </a>
+          <a>
+            About
+          </a>
+        </nav>
+      </div>
+      <div className={styles.profileContent}>
+        <Image className={styles.img} src={img} alt='myself' width={180} height={180}/>
+        <h1> Matheus Tanaka</h1>
+        <p>Hey, call me Math. <br/>Im full-stack developer and I like to build cool things</p>
+        <div className={styles.contact}>
+          <a>
+            <FiGithub className={styles.github} size={25}/>
+          </a>
+          <a>
+            <HiOutlineMail className={styles.email} size={25}/>
+          </a>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
