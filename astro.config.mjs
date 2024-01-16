0660c39f46e0c2f49b7f01061692fa23305c38fc
Matheus Tanaka <matheustanakanog@gmail.com>
2024-01-16 21:25:37 +0000

    add preact and astro islands - chapter 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://mytestblogastro.netlify.app/"
+  site: "https://mytestblogastro.netlify.app/",
+  integrations: [preact()]
 });
